34039f2f110c2fdebe8436751da7b5646d588336
liber87 <bd87@bk.ru>
2025-12-06 23:08:40 +0300

    Update setup.data.sql

--- install/setup.data.sql
+++ install/setup.data.sql
@@ -1,14 +1,13 @@
 START TRANSACTION; 
 
-CREATE TABLE `{PREFIX}site_visits` (
+CREATE TABLE `adkq_site_visits` (
   `id` int(11) NOT NULL,
   `did` int(11) NOT NULL,
   `date` varchar(10) NOT NULL,
   `ip` varchar(19) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-CREATE TABLE `{PREFIX}site_counter` (
+CREATE TABLE `adkq_site_counter` (
   `id` int(11) NOT NULL,
   `did` int(11) NOT NULL,
   `date` varchar(10) NOT NULL,
@@ -17,14 +16,12 @@ CREATE TABLE `{PREFIX}site_counter` (
   `views` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `{PREFIX}site_counter`  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;  
+ALTER TABLE `adkq_site_counter`  ADD PRIMARY KEY (`id`),  ADD UNIQUE KEY `id` (`did`,`date`);
 
-ALTER TABLE `{PREFIX}site_counter`  ADD PRIMARY KEY (`id`),  ADD UNIQUE KEY `id` (`did`,`date`);
+ALTER TABLE `adkq_site_counter`  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;  
 
-ALTER TABLE `{PREFIX}site_visits`   ADD PRIMARY KEY (`id`),  ADD UNIQUE KEY `id` (`did`,`ip`,`date`);
+ALTER TABLE `adkq_site_visits`   ADD PRIMARY KEY (`id`),  ADD UNIQUE KEY `id` (`did`,`ip`,`date`);
 
-ALTER TABLE `{PREFIX}site_visits` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+ALTER TABLE `adkq_site_visits` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 COMMIT;
-
